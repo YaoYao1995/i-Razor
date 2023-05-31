f61b3ad726cf83e6cbad8c73adf4ef6860a13acf
Yao Yao <474127934@qq.com>
2023-05-31 15:50:54 +0800

    Delete conda_init.sh

--- conda_init.sh
+++
@@ -1,8 +0,0 @@
-export PATH=/workspace/miniconda3/bin:$PATH
-apt update && apt upgrade
-apt install vim htop psmisc
-source activate
-conda activate razor
-git config --global user.email "474127934@qq.com"
-git config --global user.name "YaoYao1995"
-
